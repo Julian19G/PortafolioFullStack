3d0877c0f1281cc1c1f55c159af6e15af9befe8a
julian19G <julian19guerra@gmail.com>
2025-02-10 16:18:39 -0500

    Avances 02-10-25

--- app/(routes)/about-me/page.tsx
+++ app/(routes)/about-me/page.tsx
@@ -1,7 +1,23 @@
+import TransitionPage from "@/components/transition-page";
+import ContainerPage from "@/components/container";
+import Avatar from "@/components/avatar";
+import CounterServices from "@/components/counter-services";
+
+
 const PageAboutMe = () => {
     return (
-        <div>
-            From about me
-        </div>
+        <>
+            <TransitionPage />
+            <ContainerPage>
+                <Avatar />
+                <h1 className="text-2xl leading-light text-center md:text-left md:text-5xl md:mt-10"> Toda mi {' '}
+                    <span className="font-bold text-blue-500">trayectoria profesional</span>
+                </h1>
+
+                <CounterServices />
+            </ContainerPage>
+        </>
     );
 }
+
+export default PageAboutMe;
---
+++ components/avatar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import MotionTransition from "./transition-component";
+
+const Avatar = () => {
+    return (
+        <div>
+            <MotionTransition position="bottom" className="bottom-0 right-0 hidden md:inline-block md:absolute">
+                <Image src="/avatar-1.png" width={400} height={400}
+                className="w-full h-full" alt="Avatar" />
+            </MotionTransition>
+            
+        </div>
+      
+    );
+}
+
+export default Avatar;
